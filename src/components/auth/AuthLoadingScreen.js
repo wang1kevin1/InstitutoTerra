866005ba279fbbc079bc761d74918642c2531f63
wang1kevin1 <wang1kevin1@gmail.com>
2019-11-20 20:37:25 -0800

    Scratch/user authentication (#5)
    * Starting work on authentication
    
    * Added more to Accounts Util
    
    * Added UserAuth components and setup file structure
    
    * Changes made
    
    * Something iffy
    
    * Authentication displays
    
    * Removed AccountUtils and added Colors.js
    
    * SignIn V1 Done
    
    * Fixed sign In  unclickable field
    
    * Removed unneccessary Logo methods
    
    * clean up
    
    * Sign in activityIndicator and bugs fixed
    
    * sign up still a bit buggy
    
    * signup() buggy
    
    * Changed AWS User Pool, Sign up works
    
    * More or less most things working. Ready for pull
    
    * running standard
    
    * AuthLoading remembering user logged in

---
+++ src/components/auth/AuthLoadingScreen.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator,
+} from 'react-native'
+
+import Colors from '../../utilities/Colors'
+
+import Auth from '@aws-amplify/auth'
+
+export default class AuthLoadingScreen extends React.Component {
+  state = {
+    userToken: null
+  }
+  async componentDidMount () {
+    await this.loadApp()
+  }
+
+  // Get the logged in users and remember them
+  loadApp = async () => {
+    await Auth.currentAuthenticatedUser()
+    .then(user => {
+      this.setState({userToken: user.signInUserSession.accessToken.jwtToken})
+    })
+    .catch(err => console.log(err))
+    this.props.navigation.navigate(this.state.userToken ? 'App' : 'Auth')
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}> 
+        <ActivityIndicator size="large" color={Colors.lightblue} />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lightgreen,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
