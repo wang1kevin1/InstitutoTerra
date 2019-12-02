e3ae1fdb229dc892f516d5c0ae7460fb8ed62a02
Malcolm Neill <mneill@ucsc.edu>
2019-12-02 13:53:20 -0800

    in progress working on get distance equation

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import CarbonFootprint from './components/CarbonFootprint'
 export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <CarbonFootprint/>
     </View>
   )
@@ -14,7 +13,7 @@ export default function App () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#cfff6e',
     alignItems: 'center',
     justifyContent: 'center'
   }
