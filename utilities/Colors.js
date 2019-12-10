3b256f2d9ffca3dad91e1d640fcc5ab327f0d4c9
Malcolm Neill <mneill@ucsc.edu>
2019-12-09 18:38:19 -0800

    test

---
+++ utilities/Colors.js
@@ -0,0 +1,16 @@
+const Colors = {
+    white: '#fff',
+    black: '#000',
+  
+    darkgrey: '#6D7365',
+    grey: '#9A9A9A',
+    lightgrey: '#E0E0E0',
+  
+    green: '#648C1C',
+    lightgreen: '#CAF28D',
+    mutedgreen: '#effbdd',
+  
+    lightblue: '#82A2F4'
+  }
+  
+  export default Colors
