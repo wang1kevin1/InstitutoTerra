5e8112bf7212555a2ef3477e2aa97b8d335876f6
Malcolm Neill <mneill@ucsc.edu>
2019-12-09 18:38:19 -0800

    test

--- components/CarbonFootprint.js
+++ components/CarbonFootprint.js
@@ -1,107 +1,95 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import DistanceEmissions from './DistanceEmissions';
-
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import Colors from "../utilities/Colors.js"
 
 class CarbonFootprint extends Component {
    state = {
-      data: [],
-      iata: ''
+      iata: '',
+      isLoading: true
    }
    componentDidMount = () => {
-      fetch('http://aviation-edge.com/v2/public/flights?key=760fd0-cefe7a&flightIata=EK247', {
+      fetch('http://aviation-edge.com/v2/public/routes?key=760fd0-cefe7a&airlineIata=UA&flightnumber=510', {
          method: 'GET'
       })
       .then((response) => response.json())
       .then((responseJson) => {
-         console.log(responseJson[0].arrival.iataCode);
+         console.log(responseJson[0]);
          this.setState({
             isLoading: false,
             data: responseJson[0],
-            aircraftIata: responseJson[0].aircraft.iataCode,
-            arrivalIata: responseJson[0].arrival.iataCode,
-            departureIata: responseJson[0].departure.iataCode,
+            airlineIata: responseJson[0].airlineIata,
+            arrivalIata: responseJson[0].arrivalIata,
+            departureIata: responseJson[0].departureIata,
          })
-         return [responseJson[0].arrival.iataCode, responseJson[0].departure.iataCode]
-      }).then((IataCode) =>{
-         this.getArrivalAirport(IataCode[0]);
-         this.getDepartureAirport(IataCode[1]);
-      })
-      .catch((error) => {
-         console.error(error);
-      });
-      this.getDistance()
-   }
-
-   getArrivalAirport(aIataCode){
-       fetch(`https://aviation-edge.com/v2/public/airportDatabase?key=760fd0-cefe7a&codeIataAirport=${aIataCode}`,{
-           method: 'GET'
-       })
-       .then((response) => response.json())
-      .then((responseJson) => {
-         console.log(responseJson[0].nameAirport);
-         this.setState({
-            destinationName: responseJson[0].nameAirport,
-            destinationLat: responseJson[0].latitudeAirport,
-            destinationLong: responseJson[0].longitudeAirport
-         })
+         return [this.state.arrivalIata, this.state.departureIata]
+      }).then((IataCodes) =>{
+         this.getAirports(IataCodes);
       })
       .catch((error) => {
          console.error(error);
       });
    }
-   getDepartureAirport(dIataCode){
-      fetch(`https://aviation-edge.com/v2/public/airportDatabase?key=760fd0-cefe7a&codeIataAirport=${dIataCode}`,{
-          method: 'GET'
-      })
-      .then((response) => response.json())
-     .then((responseJson) => {
-        console.log(responseJson[0].nameAirport);
-        this.setState({
-           departureName: responseJson[0].nameAirport,
-           departureLat: responseJson[0].latitudeAirport,
-           departureLong: responseJson[0].longitudeAirport,
-        })
-     })
-     .catch((error) => {
-        console.error(error);
-     });
 
-  }
+   getAirports(IataCode){
+      let arrAirportCall = fetch(`https://aviation-edge.com/v2/public/airportDatabase?key=760fd0-cefe7a&codeIataAirport=${IataCode[0]}`);
+      let arrCityCall = fetch(`https://aviation-edge.com/v2/public/cityDatabase?key=760fd0-cefe7a&codeIataCity=${IataCode[0]}`)
+      let depAirportCall = fetch(`https://aviation-edge.com/v2/public/airportDatabase?key=760fd0-cefe7a&codeIataAirport=${IataCode[1]}`);
+      let depCityCall = fetch(`https://aviation-edge.com/v2/public/cityDatabase?key=760fd0-cefe7a&codeIataCity=${IataCode[1]}`)
+
+      Promise.all([arrAirportCall, depAirportCall,  arrCityCall, depCityCall])
+         .then(values => Promise.all(values.map(value => value.json())))
+         .then(response => {
+            this.setState({
+               arrCityIata: response[0][0].codeIataCity,
+               arrCityName: response[2][0].nameCity,
+               arrLat: response[0][0].latitudeAirport,
+               arrLong: response[0][0].longitudeAirport,
+               depCityIata: response[1][0].codeIataCity,
+               depCityName: response[3][0].nameCity,
+               depLat: response[1][0].latitudeAirport,
+               depLong: response[1][0].longitudeAirport
+            })
+            console.log(response[2])
+            console.log(this.state.arrCityName);
+            console.log(this.state.depCityIata);
+            this.getDistance();  
+         });
+   }
+   toRad(x){
+      let pi = Math.PI;
+      return x * (pi/180);
+     }
   getDistance(){
-     let x = this.state.destinationLat
-     console.log("should have lat here")
-     console.log(this.state.destinationLat)
+     let earthRad = 6731;
+     let latDiff = this.toRad(this.state.arrLat - this.state.depLat);
+     let longDiff = this.toRad(this.state.arrLong - this.state.depLong);
+     let latArr  = this.state.arrLat;
+     let latDep = this.state.depLat
+     let a = Math.sin(latDiff/2) * Math.sin(latDiff/2) + Math.sin(longDiff/2)*Math.sin(longDiff/2) * Math.cos(latArr) * Math.cos(latDep);
+     let c = 2 *  Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+     let d =  Math.round(earthRad * c);
+     console.log("should have distance here")
+     console.log(d)
      this.setState({
-        distanceTraveled: x,
-     })
+        distanceTraveled: d,
+     });
   }
+  
 
    render() {
       return (
-         <View style={styles.container}>
-            <View  height="15%">
-            <Text padding='10%'>
-               {this.state.aircraftIata}
-            </Text>
-            <Text>
-               {this.state.arrivalIata}
-               {this.state.destinationName}
-               {this.state.destinationLat}
-               {this.state.departureName}
-                
-            </Text>
-            <Text>
-               {this.state.distanceTraveled}
-            </Text>
-            <DistanceEmissions
-               dLat={this.state.departureLat}
-               dLong={this.state.departureLong}
-               aLat={this.state.destinationLat}
-               aLong={this.state.destinationLong}
-            />
+         <SafeAreaView>
+            <View style={styles.container}>
+               <View  height="15%">
+                  <Text padding='10%'>
+                     Flight Number: from {this.state.depCityName} to {this.state.arrCityName}
+                  </Text>
+                  <Text>
+                     This trip was over {this.state.distanceTraveled} km
+                  </Text>
+               </View>
             </View>
-         </View>
+         </SafeAreaView>
       )
    }
 }
@@ -110,9 +98,7 @@ const styles = StyleSheet.create({
       flex: 1,
       flexDirection: 'column',
       justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#ffffff',
-      height:  '15%',
+      backgroundColor: Colors.white,
     }
 });
 export default CarbonFootprint
