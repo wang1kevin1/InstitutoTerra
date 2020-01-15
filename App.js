81154933b64946a936879a200cdcb40552bdba4e
wang1kevin1 <wang1kevin1@gmail.com>
2020-01-14 18:11:29 -0800

    Scratch/calculate emissions (#12)
    * call working
    
    * in progress working on get distance equation
    
    * test
    
    * call working
    
    * in progress working on get distance equation
    
    * test
    
    * post rebase
    
    * call working
    
    * in progress working on get distance equation
    
    * test
    
    * post rebase
    
    * working API Calls  and error handling done
    
    * removing test files
    
    * added comments
    
    * Cleaning up code, changing file names, etc
    
    * mid changes, needs bug fixes
    
    * distance rounded to hundreth
    
    * Buttons on FLightInfo all work
    
    * Added dynamic button opacity & distance modifier
    
    * App.js preemptively loads font for all children
    
    * added loading screen when retrieving flight info
    
    * New footer cmpnt.  Replaced existing +add to new
    
    * Extended top button bar, added plce for nav btns
    
    * FlightInfo Screen COMPLETED
    
    * CarbonEmissions, working. Adding dynamic UI next
    
    * Edge case: can no longer select negative trees
    
    * Fixing asset file structure
    
    * added a new background for future
    
    * changes to cost for future changes/inflation
    
    * still updating render late
    
    * Years calculates and updates correctly
    
    * Color changes working
    
    * Colors and icons updated
    
    * roundtrip distance passed as well now
    
    * Added NoFlight donations screen, adjusted stack
    
    * Renamed a bunch of screens, working on c02 screen
    
    * First three screen of flight calc workflow done
    
    * file name changes to make flow more understandable
    
    * better file structure
    
    * fixed checkoutwithflight
    
    * Added flight receipt page
    
    * Receipt with flight done
    
    * update
    
    * one more update
    
    * Calculate emissions work flow completed
    
    * fixed carbon emissions non visible on iphone
    
    * changed some stuff for easier UI fixing
    
    * testing emissions page fix
    
    * check size again
    
    * possible fix?
    
    * flight info fixed
    
    * flow with flight UI fixed
    
    * footer changes
    
    * noflight receipt needs some changes.
    
    * using height for carbonemissions page
    
    * kinda fixed receipt noflight
    
    * All checkout flow UI done
    
    * testing dimensions
    
    * Test on small screens
    
    * Hopefully final fix of checkout UI
    
    * changed footer to scale as well
    
    * starting on home page, switching branch after pull
    
    * margin changes please dear god work
    
    * Revert "margin changes please dear god work"
    
    This reverts commit 547215a0a06ba9ae9fc0b89265344b2a8a022e1a.
    
    * try now please
    
    * please try now
    
    * test first page
    
    * FINALLY WORKING UI
    
    * safeareaview removed
    
    Co-authored-by: malcolmneill <44383128+malcolmneill@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import * as Font from 'expo-font';
+
 import {
   createSwitchNavigator, 
   createAppContainer,
@@ -16,8 +18,18 @@ import SettingsScreen from './src/components/auth/SettingsScreen'
 // DashboardStack
 import UserDashboardScreen from './src/components/user/UserDashboardScreen'
 
-// Main stack
+// MainStack head
 import HomeScreen from './src/components/main/HomeScreen'
+
+// FlightStack
+import FlightInfoScreen from './src/components/main/flight/FlightInfoScreen'
+import CarbonEmissionsScreen from './src/components/main/flight/CarbonEmissionsScreen'
+import CheckoutWithFlightScreen from './src/components/main/flight/CheckoutWithFlightScreen'
+import ReceiptWithFlightScreen from './src/components/main/flight/ReceiptWithFlightScreen'
+
+// NoFlightStack
+import CheckoutWithoutFlightScreen from './src/components/main/noFlight/CheckoutWithoutFlightScreen'
+import ReceiptWithoutFlightScreen from './src/components/main/noFlight/ReceiptWithoutFlightScreen'
 
 // Amplify imports and config
 import Amplify from '@aws-amplify/core'
@@ -31,9 +43,25 @@ const AuthStackNavigator = createStackNavigator({
   ForgotPassword: ForgotPasswordScreen,
 }, { headerMode: 'none' })
 
+// Flight Stack
+const FlightStackNavigator = createStackNavigator({
+  FlightInfo: FlightInfoScreen,
+  CarbonEmissions: CarbonEmissionsScreen,
+  CheckoutWithFlight: CheckoutWithFlightScreen,
+  ReceiptWithFlight: ReceiptWithFlightScreen,
+}, { headerMode: 'none' })
+
+// NoFlight Stack
+const NoFlightStackNavigator = createStackNavigator({
+  CheckoutWithoutFlight: CheckoutWithoutFlightScreen,
+  ReceiptWithoutFlight: ReceiptWithoutFlightScreen,
+}, { headerMode: 'none' })
+
 // Main stack
 const MainStackNavigator = createStackNavigator({
   Home: HomeScreen,
+  Flight: FlightStackNavigator, // FlightStack
+  NoFlight: NoFlightStackNavigator, // NoFlightStack
   UserDashboard: UserDashboardScreen, // DashboardStack
   Settings: SettingsScreen, 
   Auth: AuthStackNavigator, // AuthStackNavigator
@@ -43,6 +71,20 @@ const AppSwitchNavigator = createSwitchNavigator({
   Main: MainStackNavigator, // the MainStack
 })
 
-const App = createAppContainer(AppSwitchNavigator)
+const AppContainer = createAppContainer(AppSwitchNavigator)
 
-export default App
+export default class App extends React.Component {
+  componentDidMount = () => {
+    Font.loadAsync({
+      'Montserrat': require('./src/assets/fonts/Montserrat-Regular.ttf'),
+      'Montserrat-bold': require('./src/assets/fonts/Montserrat-Bold.ttf'),
+      'Fago-black': require('./src/assets/fonts/Fago-Black.ttf'),
+    });
+  }
+
+  render() {
+    return(
+      <AppContainer/>
+    )
+  }
+}
---
+++ src/components/main/flight/CheckoutWithFlightScreen.js
@@ -0,0 +1,341 @@
+import React from 'react'
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native'
+
+import { Ionicons, FontAwesome, Feather } from '@expo/vector-icons';
+
+import Dash from 'react-native-dash';
+
+import Colors from '../../../assets/Colors.js';
+
+import Footer from '../../utilities/Footer.js';
+
+import Auth from '@aws-amplify/auth';
+
+// cost per tree
+const cost = 6;
+
+export default class CheckoutWithFlightScreen extends React.Component {
+  state = {
+    isAuthenticated: 'false',
+    data: [],
+    treeNum: 0,
+    total_cost: 0,
+    color: Colors.grey
+  }
+
+  componentDidMount = () => {
+    //set state parameters
+    this.setState({
+      tripIndex: this.props.navigation.getParam('tripIndex', 'tripIndex'),
+      depCityName: this.props.navigation.getParam('depCityName', 'departureCity'),
+      arrCityName: this.props.navigation.getParam('arrCityName', 'arrivalCity'),
+      footprint: this.props.navigation.getParam('footprint', 'carbonEmissions'),
+      flightChars: this.props.navigation.getParam('flightChars', 'chars'),
+      flightNums: this.props.navigation.getParam('flightNums', 'nums'),
+    })
+    this.checkAuth()
+  }
+
+  // Checks if a user is logged in
+  async checkAuth() {
+    await Auth.currentAuthenticatedUser({ bypassCache: true })
+      .then(() => {
+        console.log('A user is logged in')
+        this.setState({ isAuthenticated: true })
+      })
+      .catch(err => {
+        console.log('Nobody is logged in')
+        this.setState({ isAuthenticated: false })
+      })
+  }
+
+  // Sends user to sign up or dashboard depending on Auth state
+  handleUserRedirect() {
+    if (this.state.isAuthenticated) {
+      this.props.navigation.navigate('UserDashboard')
+    } else {
+      this.props.navigation.navigate('SignIn')
+    }
+  }
+
+  // handle color variants
+  colorVariant() {
+    switch (this.state.treeNum) {
+      case 0:
+        return (Colors.grey)
+      case 1:
+        return (Colors.greygreen1)
+      case 2:
+        return (Colors.greygreen2)
+      case 3:
+        return (Colors.greygreen3)
+      case 4:
+        return (Colors.greygreen4)
+      default:
+        return (Colors.lightgreen)
+    }
+  }
+
+  //handle checkout redirect
+  handleCheckout() {
+    if (this.state.treeNum != 0) {
+      this.props.navigation.navigate('ReceiptWithFlight', {
+        tripIndex: this.state.tripIndex,
+        depCityName: this.state.depCityName,
+        arrCityName: this.state.arrCityName,
+        footprint: this.state.footprint,
+        treeNum: this.state.treeNum,
+        years: this.calcYears(),
+        total_cost: this.state.total_cost,
+        flightChars: this.state.flightChars,
+        flightNums: this.state.flightNums,
+      })
+    }
+  }
+
+  // handle add
+  handleAdd() {
+    this.setState({
+      treeNum: this.state.treeNum + 1,
+      total_cost: this.state.total_cost + cost
+    })
+  }
+
+  // handle remove
+  handleRemove() {
+    if (this.state.treeNum != 0) {
+      this.setState({
+        treeNum: this.state.treeNum - 1,
+        total_cost: this.state.total_cost - cost
+      })
+    }
+  }
+
+  //Calculate years to neutralize emission footprint
+  calcYears() {
+    tempY = Math.round(this.state.footprint * 5 / this.state.treeNum);
+    return (tempY)
+  }
+
+  render() {
+    const {
+      flightChars,
+      flightNums,
+      treeNum,
+      footprint,
+      total_cost,
+    } = this.state;
+
+    const years = this.calcYears()
+    const color = this.colorVariant()
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.containerTop}>
+          <View style={styles.buttonBarNav}>
+            {/*Navigation Buttons*/}
+            <Ionicons style={styles.navigationIcon} name="md-arrow-back"
+              onPress={() => this.props.navigation.goBack()} />
+            <Text style={styles.midBlueText}>FLIGHT {flightChars} {flightNums}</Text>
+            <FontAwesome style={styles.navigationIcon} name="user-circle-o"
+              onPress={() => this.handleUserRedirect()} />
+          </View>
+          <View style={styles.topText}>
+            {/*CO2 footprint*/}
+            <Text style={styles.bigGreyText}>{footprint}</Text>
+            <View style={styles.alignSubScript}>
+              <Text style={styles.midGreyText}>METRIC TONS CO</Text>
+              <Text style={{ fontSize: 10, lineHeight: 30, color: Colors.darkgrey }}>2</Text>
+            </View>
+          </View>
+          <View style={styles.iterateGroup}>
+            {/*Subtract tree*/}
+            <TouchableOpacity
+              style={[styles.iterators, { backgroundColor: (this.state.treeNum == 0) ? Colors.grey : Colors.lightgreen }]}
+              onPress={() => this.handleRemove()}>
+              <Feather style={[styles.iteratorIcon, { color: (this.state.treeNum == 0) ? Colors.white : Colors.lightblue }]} name="minus" />
+            </TouchableOpacity>
+            {/*Tree counter*/}
+            <View style={[styles.treeCounter, { backgroundColor: color }]}>
+              <Text style={styles.treeCountText}>{treeNum}</Text>
+            </View>
+            {/*Add tree*/}
+            <TouchableOpacity
+              style={[styles.iterators, { backgroundColor: Colors.lightgreen }]}
+              onPress={() => this.handleAdd()}>
+              <Feather style={[styles.iteratorIcon, { color: Colors.lightblue }]} name="plus" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.bottomText}>
+            {/*Years to neutralize carbon footprint*/}
+            <Text style={styles.midBlueText}>YEARS TO NEUTRALIZE</Text>
+            {years != Infinity &&
+              <Text style={styles.bigBlueText}>{years}</Text>
+            }
+            {years == Infinity &&
+              <Text style={styles.bigBlueText}>&#x2014;</Text>
+            }
+          </View>
+          <Dash style={styles.dashedLine} dashColor={Colors.lightgrey} dashGap={0} />
+          <View style={styles.receiptContainer}>
+            <View style={styles.textRow}>
+              {/*Total trees donated in transaction*/}
+              <Text style={styles.receiptTextLeft}>TOTAL TREES</Text>
+              <Text style={styles.receiptTextRight}>{treeNum}</Text>
+            </View>
+            <View style={styles.textRow}>
+              {/*Cost of transaction*/}
+              <Text style={styles.receiptTextLeft}>PRICE</Text>
+              <Text style={styles.receiptTextRight}>${total_cost}</Text>
+            </View>
+          </View>
+          {/*Navigate to checkout page*/}
+          <TouchableOpacity
+            style={[styles.bottomGreenButton, { backgroundColor: (this.state.treeNum == 0) ? Colors.grey : Colors.lightgreen }]}
+            onPress={() => this.handleCheckout()}>
+            <Text style={styles.buttonText}>CHECKOUT</Text>
+          </TouchableOpacity>
+        </View>
+        <Footer color='white' />
+      </View>
+    )
+  }
+}
+
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: Colors.white,
+    height: height,
+    width: width
+  },
+  containerTop: {
+    paddingLeft: width * 0.05,
+    paddingRight: width * 0.05,
+    paddingTop: height * 0.06,
+    marginBottom: height * 0.10,
+    backgroundColor: Colors.white,
+  },
+  buttonBarNav: {
+    flexDirection: 'row',
+    height: height * 0.05,
+    justifyContent: 'space-between',
+    marginBottom: height * 0.03,
+  },
+  topText: {
+    alignItems: 'center',
+  },
+  bottomText: {
+    alignItems: 'center',
+  },
+  alignSubScript: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  midBlueText: {
+    fontFamily: 'Montserrat',
+    fontSize: 14,
+    color: Colors.lightblue,
+    alignItems: 'center',
+    lineHeight: 30
+  },
+  bigBlueText: {
+    fontFamily: 'Montserrat-bold',
+    fontSize: 25,
+    color: Colors.lightblue,
+    lineHeight: 30
+  },
+  midGreyText: {
+    fontFamily: 'Montserrat',
+    fontSize: 14,
+    color: Colors.darkgrey,
+    lineHeight: 25
+  },
+  bigGreyText: {
+    fontFamily: 'Montserrat-bold',
+    fontSize: 20,
+    color: Colors.darkgrey,
+    lineHeight: 25
+  },
+  iterateGroup: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: height * 0.25,
+  },
+  iterators: {
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width * 0.10,
+    aspectRatio: 1/1
+  },
+  iteratorIcon: {
+    fontSize: 25,
+  },
+  treeCounter: {
+    marginLeft: width * 0.20,
+    marginRight: width * 0.20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width * 0.30,
+    aspectRatio: 3/4,
+    borderRadius: 25,
+  },
+  treeCountText: {
+    fontFamily: 'Montserrat-bold',
+    color: Colors.white,
+    fontSize: 50
+  },
+  receiptContainer: {
+    marginBottom: height * 0.07
+  },
+  receiptTextLeft: {
+    fontFamily: 'Montserrat',
+    color: Colors.darkgrey,
+    fontSize: 12
+  },
+  receiptTextRight: {
+    fontFamily: 'Montserrat-bold',
+    color: Colors.darkgrey,
+    fontSize: 12
+  },
+  dashedLine: {
+    width: '100%',
+    height: 1,
+    marginTop: height * 0.025,
+    marginBottom: height * 0.02
+  },
+  textRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: height * 0.02,
+  },
+  bottomGreenButton: {
+    borderRadius: 10,
+    backgroundColor: Colors.lightgreen,
+    height: height * 0.08,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontFamily: 'Montserrat-bold',
+    color: Colors.darkgrey,
+    fontSize: 12
+  },
+  navigationIcon: {
+    color: Colors.grey,
+    fontSize: 30,
+  },
+});
+
