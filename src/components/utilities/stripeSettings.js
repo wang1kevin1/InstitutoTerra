6e8fc2bcbe989998b78a184cc00f8c6f512c243f
Kevin Wang <wang1kevin1@gmail.com>
2020-01-29 16:04:10 -0800

    test

--- src/components/utilities/stripeSettings.js
+++ src/components/utilities/stripeSettings.js
@@ -1,6 +1,6 @@
 export const STRIPE = {
   PUBLIC_KEY: 'pk_test_BH7KSQ3Fi4OnbVNcB5t6P2PR00cBZApc9f',
-  PLAN_NAME: 'Tree',
+  SKU: 'sku_GcvolINXQ7ltwi',
   SUCCESS_URL: 'https://yourwebsite/payment/success',
   CANCELED_URL: 'https://yourwebsite/payment/canceled',
 };
