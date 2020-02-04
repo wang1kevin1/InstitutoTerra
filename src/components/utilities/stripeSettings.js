953a500838da475ed15c9b310b7918d731da68fe
Kevin Wang <wang1kevin1@gmail.com>
2020-02-03 21:03:22 -0800

    Kinda works. need to fx urls

--- src/components/utilities/stripeSettings.js
+++ src/components/utilities/stripeSettings.js
@@ -1,6 +1,6 @@
 export const STRIPE = {
   PUBLIC_KEY: 'pk_test_BH7KSQ3Fi4OnbVNcB5t6P2PR00cBZApc9f',
   SKU: 'sku_GcvolINXQ7ltwi',
-  SUCCESS_URL: 'https://yourwebsite/payment/success',
-  CANCELED_URL: 'https://yourwebsite/payment/canceled',
+  SUCCESS_URL: 'https://www.example.com/success',
+  CANCELED_URL: 'https://www.example.com/cancel',
 };
