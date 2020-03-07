3f5c1038ddace2af80d446cb663648be2b69360e
Kevin Wang <wang1kevin1@gmail.com>
2020-03-06 21:37:09 -0800

    refactored about

--- src/components/main/AboutScreen.js
+++ src/components/main/AboutScreen.js
@@ -18,8 +18,6 @@ export default class AboutScreen extends React.Component {
   constructor(props) {
     super(props)
 
-    this.props.navigation.getParam('color', 'color')
-
     this.icon_green = require('../../assets/icons/icon-green.png')
     this.icon_black = require('../../assets/icons/icon-black.png')
 
@@ -32,135 +30,75 @@ export default class AboutScreen extends React.Component {
   }
 
   render() {
-    if (this.props.navigation.getParam('color', 'color') == 'green') {
-      return (
-        <View style={styles.container}>
-          <View style={styles.backContainer}>
-            <View style={styles.headerContainer}>
-              <Text style={styles.aboutText}>ABOUT INSTITUTO TERRA</Text>
-              <Ionicons style={styles.navigationIcon} name="md-close" onPress={() => this.props.navigation.goBack()} />
-            </View>
-            <ScrollView
-              showsVerticalScrollIndicator={false}>
-              <View style={styles.mainContainer}>
-                <Image source={this.icon_green} style={styles.logo} />
-                <Image source={this.terra_green} style={styles.terra} />
-                <View style={styles.whiteline} />
-                <Text style={styles.normalText}>Since its creation in 1998, the Instituto Terra has had three focal principles:</Text>
-              </View>
-              <View style={styles.threePrinciples}>
-                <View style={styles.numbers}>
-                  <Text style={styles.numberText}>1</Text>
-                  <Text style={styles.numberText}>2</Text>
-                  <Text style={styles.numberText}>3</Text>
-                </View>
-                <View style={styles.descriptions}>
-                  <Text style={styles.descText}>The recovery and restoration of its founding seat: the RPPN Bulcão farm.</Text>
-                  <Text style={styles.descText}>The spreading of ecological values through educational pograms.</Text>
-                  <Text style={styles.descText}>The recovery and restoration of rural properties in the valley of the Rio Doce.</Text>
-                </View>
-              </View>
-              <Image source={this.about_1} style={styles.imageContainer1} />
-              <View style={styles.greenBubbleContainer}>
-                <Text style={styles.bubbleText}>In its 20 years of operation the Instituto Terra has successfully planted over 2.7 million trees.</Text>
-              </View>
-              <View style={styles.textContainer}>
-                <Text style={styles.normalText}>The revival of our ecosystem has brought back a great variety of animal species; 172
-                species of birds, 33 species of mammals, 16 species of reptiles and 16 species of amphibians. Our vivarium has a capacity
-                of producing 600,000 saplings a year and our laboratory guarantees us the technical knowlege to produce 297 species of
-                trees native to our region.</Text>
-              </View>
-              <Image source={this.about_2} style={styles.imageContainer2} />
-              <View style={styles.textContainer}>
-                <Text style={styles.normalText}>In its educational role the Institute established the goal of disseminating its
-                ecological values, mainly through tree complementary programs.</Text>
-              </View>
-              <View style={styles.greenBubbleContainer}>
-                <Text style={styles.bubbleText}>More than 82,000 students, primarily from the regions of Rio Doce, have graduated from
-                several courses and programs offered by the Institute.</Text>
-              </View><View style={styles.textContainer}>
-                <Text style={styles.normalText}>The 'Terrinha' project has the goal of making students between 11 and 13 years of age
-                become conscious of ecological themes. 760 children partook in this program and will pass on the knowledge to their
-                respective classrooms, indirectly affecting over 19,000 students in the region. Since 2005, 177 young agricultural technicians
-              were trained in the recovery of decraded areas through a boarding program that lasts 1 year.</Text>
-              </View>
-              <Image source={this.about_3} style={styles.imageContainer3} />
-              <View style={styles.normalText}>
-                <Text style={styles.normalText}>The institute has recovered 1977 springs so far, this has increased water flow and provoked
-                a systematic improvement in the productivity of approximately 49,425 hectares of land belonging to 1044 low income families.
-                This project has become the main focus for the Institute.</Text>
-              </View>
-              <View style={styles.footer}>
-              </View>
-            </ScrollView>
+    return (
+      <View style={styles.container}>
+        <View style={[styles.backContainer,
+        { backgroundColor: (this.props.navigation.getParam('color', 'color') == 'green') ? COLORS.green : COLORS.darkgrey }]}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.aboutText}>ABOUT INSTITUTO TERRA</Text>
+            <Ionicons style={styles.navigationIcon} name="md-close" onPress={() => this.props.navigation.goBack()} />
           </View>
-        </View>
-      )
-    }
-    else {
-      return (
-        <View style={styles.container}>
-          <View style={styles.darkBackContainer}>
-            <View style={styles.headerContainer}>
-              <Text style={styles.aboutText}>ABOUT INSTITUTO TERRA</Text>
-              <Ionicons style={styles.navigationIcon} name="md-close" onPress={() => this.props.navigation.goBack()} />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.mainContainer}>
+              <Image
+                source={(this.props.navigation.getParam('color', 'color') == 'green') ? this.icon_green : this.icon_black}
+                style={styles.logo} />
+              <Image
+                source={(this.props.navigation.getParam('color', 'color') == 'green') ? this.terra_green : this.terra_black}
+                style={styles.terra} />
+              <View style={styles.whiteline} />
+              <Text style={(this.props.navigation.getParam('color', 'color') == 'green') ? styles.normalText : styles.greenNormalText}>
+                Since its creation in 1998, the Instituto Terra has had three focal principles:
+                </Text>
             </View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-              <View style={styles.mainContainer}>
-                <Image source={this.icon_black} style={styles.logo} />
-                <Image source={this.terra_black} style={styles.terra} />
-                <View style={styles.whiteline} />
-                <Text style={styles.greenNormalText}>Since its creation in 1998, the Instituto Terra has had three focal principles:</Text>
+            <View style={styles.threePrinciples}>
+              <View style={styles.numbers}>
+                <Text style={(this.props.navigation.getParam('color', 'color') == 'green') ? styles.numberText : styles.greenNumberText}>1</Text>
+                <Text style={(this.props.navigation.getParam('color', 'color') == 'green') ? styles.numberText : styles.greenNumberText}>2</Text>
+                <Text style={(this.props.navigation.getParam('color', 'color') == 'green') ? styles.numberText : styles.greenNumberText}>3</Text>
               </View>
-              <View style={styles.threePrinciples}>
-                <View style={styles.numbers}>
-                  <Text style={styles.numberText}>1</Text>
-                  <Text style={styles.numberText}>2</Text>
-                  <Text style={styles.numberText}>3</Text>
-                </View>
-                <View style={styles.descriptions}>
-                  <Text style={styles.descText}>The recovery and restoration of its founding seat: the RPPN Bulcão farm.</Text>
-                  <Text style={styles.descText}>The spreading of ecological values through educational pograms.</Text>
-                  <Text style={styles.descText}>The recovery and restoration of rural properties in the valley of the Rio Doce.</Text>
-                </View>
+              <View style={styles.descriptions}>
+                <Text style={styles.descText}>The recovery and restoration of its founding seat: the RPPN Bulcão farm.</Text>
+                <Text style={styles.descText}>The spreading of ecological values through educational pograms.</Text>
+                <Text style={styles.descText}>The recovery and restoration of rural properties in the valley of the Rio Doce.</Text>
               </View>
-              <Image source={this.about_1} style={styles.imageContainer1} />
-              <View style={styles.darkBubbleContainer}>
-                <Text style={styles.bubbleText}>In its 20 years of operation the Instituto Terra has successfully planted over 2.7 million trees.</Text>
-              </View>
-              <View style={styles.textContainer}>
-                <Text style={styles.normalText}>The revival of our ecosystem has brought back a great variety of animal species; 172
-                species of birds, 33 species of mammals, 16 species of reptiles and 16 species of amphibians. Our vivarium has a capacity
-                of producing 600,000 saplings a year and our laboratory guarantees us the technical knowlege to produce 297 species of
+            </View>
+            <Image source={this.about_1} style={styles.imageContainer1} />
+            <View style={[styles.bubbleContainer, { backgroundColor: (this.props.navigation.getParam('color', 'color') == 'green') ? COLORS.darkgreen : COLORS.lightblack }]}>
+              <Text style={styles.bubbleText}>In its 20 years of operation the Instituto Terra has successfully planted over 2.7 million trees.</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.normalText}>The revival of our ecosystem has brought back a great variety of animal species; 172
+              species of birds, 33 species of mammals, 16 species of reptiles and 16 species of amphibians. Our vivarium has a capacity
+              of producing 600,000 saplings a year and our laboratory guarantees us the technical knowlege to produce 297 species of
                 trees native to our region.</Text>
-              </View>
-              <Image source={this.about_2} style={styles.imageContainer2} />
-              <View style={styles.textContainer}>
-                <Text style={styles.normalText}>In its educational role the Institute established the goal of disseminating its
+            </View>
+            <Image source={this.about_2} style={styles.imageContainer2} />
+            <View style={styles.textContainer}>
+              <Text style={styles.normalText}>In its educational role the Institute established the goal of disseminating its
                 ecological values, mainly through tree complementary programs.</Text>
-              </View>
-              <View style={styles.darkBubbleContainer}>
-                <Text style={styles.bubbleText}>More than 82,000 students, primarily from the regions of Rio Doce, have graduated from
+            </View>
+            <View style={[styles.bubbleContainer, { backgroundColor: (this.props.navigation.getParam('color', 'color') == 'green') ? COLORS.darkgreen : COLORS.lightblack }]}>
+              <Text style={styles.bubbleText}>More than 82,000 students, primarily from the regions of Rio Doce, have graduated from
                 several courses and programs offered by the Institute.</Text>
-              </View><View style={styles.textContainer}>
-                <Text style={styles.normalText}>The 'Terrinha' project has the goal of making students between 11 and 13 years of age
-                become conscious of ecological themes. 760 children partook in this program and will pass on the knowledge to their
-                respective classrooms, indirectly affecting over 19,000 students in the region. Since 2005, 177 young agricultural technicians
+            </View><View style={styles.textContainer}>
+              <Text style={styles.normalText}>The 'Terrinha' project has the goal of making students between 11 and 13 years of age
+              become conscious of ecological themes. 760 children partook in this program and will pass on the knowledge to their
+              respective classrooms, indirectly affecting over 19,000 students in the region. Since 2005, 177 young agricultural technicians
               were trained in the recovery of decraded areas through a boarding program that lasts 1 year.</Text>
-              </View>
-              <Image source={this.about_3} style={styles.imageContainer3} />
-              <View style={styles.normalText}>
-                <Text style={styles.normalText}>The institute has recovered 1977 springs so far, this has increased water flow and provoked
-                a systematic improvement in the productivity of approximately 49,425 hectares of land belonging to 1044 low income families.
+            </View>
+            <Image source={this.about_3} style={styles.imageContainer3} />
+            <View style={styles.normalText}>
+              <Text style={styles.normalText}>The institute has recovered 1977 springs so far, this has increased water flow and provoked
+              a systematic improvement in the productivity of approximately 49,425 hectares of land belonging to 1044 low income families.
                 This project has become the main focus for the Institute.</Text>
-              </View>
-              <View style={styles.footer}>
-              </View>
-            </ScrollView>
-          </View>
+            </View>
+            <View style={styles.footer}>
+            </View>
+          </ScrollView>
         </View>
-      )
-    }
+      </View>
+    )
   }
 }
 
@@ -175,16 +113,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   backContainer: {
-    backgroundColor: COLORS.green,
-    top: height * .05,
-    width: width,
-    height: height,
-    borderRadius: 25,
-    padding: width * .03,
-    flexDirection: "column"
-  },
-  darkBackContainer: {
-    backgroundColor: COLORS.darkgrey,
     top: height * .05,
     width: width,
     height: height,
@@ -202,14 +130,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  navigationIcon: {
-    color: COLORS.white,
-    fontSize: 30,
-  },
   aboutText: {
     fontSize: 12,
     fontFamily: 'Montserrat',
     color: COLORS.white,
+  },
+  navigationIcon: {
+    color: COLORS.white,
+    fontSize: 30,
   },
   mainContainer: {
     backgroundColor: 'transparent',
@@ -227,6 +155,75 @@ const styles = StyleSheet.create({
     width: width * 0.8,
     height: height * 0.1,
     resizeMode: 'contain',
+  },
+  whiteline: {
+    height: height * .03,
+    borderTopColor: COLORS.white,
+    borderTopWidth: 2,
+    width: width * .9,
+  },
+  textContainer: {
+    padding: width * .05,
+  },
+  threePrinciples: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "stretch",
+    height: height * .3,
+    paddingTop: height * .03,
+    backgroundColor: "transparent"
+  },
+  numbers: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    height: height * 0.25
+  },
+  numberText: {
+    fontFamily: 'Montserrat',
+    fontSize: 35,
+    color: COLORS.white,
+    textAlign: "center"
+  },
+  greenNumberText: {
+    fontFamily: 'Montserrat',
+    fontSize: 35,
+    color: COLORS.lightgreen,
+    textAlign: "center"
+  },
+  descriptions: {
+    flex: 6,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    height: height * 0.25
+  },
+  descText: {
+    fontFamily: 'Montserrat',
+    fontSize: 14,
+    color: COLORS.white,
+    textAlign: "left",
+  },
+  normalText: {
+    fontFamily: 'Montserrat',
+    fontSize: 15,
+    color: COLORS.white,
+    textAlign: "center",
+  },
+  greenNormalText: {
+    fontFamily: 'Montserrat',
+    fontSize: 15,
+    color: COLORS.lightgreen,
+    textAlign: "center",
+  },
+  bubbleContainer: {
+    padding: width * .05,
+    borderRadius: 25,
+  },
+  bubbleText: {
+    fontFamily: 'Montserrat-bold',
+    fontSize: 15,
+    color: COLORS.lightgreen,
+    textAlign: "center",
   },
   imageContainer1: {
     borderRadius: 25,
@@ -250,79 +247,6 @@ const styles = StyleSheet.create({
     resizeMode: 'stretch',
     padding: width * .05,
     marginBottom: height * .02
-  },
-  headerBar: {
-    height: height,
-    backgroundColor: COLORS.green,
-  },
-  whiteline: {
-    height: height * .03,
-    borderTopColor: COLORS.white,
-    borderTopWidth: 2,
-    width: width * .9,
-  },
-  normalText: {
-    fontFamily: 'Montserrat',
-    fontSize: 15,
-    color: COLORS.white,
-    textAlign: "center",
-  },
-  greenNormalText: {
-    fontFamily: 'Montserrat',
-    fontSize: 15,
-    color: COLORS.lightgreen,
-    textAlign: "center",
-  },
-  bubbleText: {
-    fontFamily: 'Montserrat-bold',
-    fontSize: 15,
-    color: COLORS.lightgreen,
-    textAlign: "center",
-  },
-  textContainer: {
-    padding: width * .05,
-  },
-  threePrinciples: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "stretch",
-    height: height * .3,
-    paddingTop: height * .03,
-    backgroundColor: "transparent"
-  },
-  numbers: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-around",
-    height: height*0.25
-  },
-  numberText: {
-    fontFamily: 'Montserrat',
-    fontSize: 35,
-    color: COLORS.white,
-    textAlign: "center"
-  },
-  descriptions: {
-    flex: 6,
-    flexDirection: "column",
-    justifyContent: "space-around",
-    height: height*0.25
-  },
-  descText: {
-    fontFamily: 'Montserrat',
-    fontSize: 14,
-    color: COLORS.white,
-    textAlign: "left",
-  },
-  greenBubbleContainer: {
-    padding: width * .05,
-    backgroundColor: COLORS.darkgreen,
-    borderRadius: 25,
-  },
-  darkBubbleContainer: {
-    padding: width * .05,
-    backgroundColor: COLORS.lightblack,
-    borderRadius: 25,
   },
   footer: {
     backgroundColor: "transparent",
--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -14,6 +14,7 @@ import COLORS from '../../assets/Colors.js'
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       color: '',
     }
