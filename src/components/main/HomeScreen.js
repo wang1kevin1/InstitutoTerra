dc2f248365d1d9b7d66682ec6860025f703985cc
George Mbugua <gmbugua2020@gmail.com>
2020-06-25 22:10:24 -0700

    REF-3: Home Screen Redesign New (#42)
    * App.js modified with correct and only background image path. Menu Bar on home screen in the works.
    
    * Menu Bar on HomeScreen_alt.js implemented, HomeScreen import  on App.js changed to path to HomeScreen_alt
    
    * Margin shifted right
    
    * Margin shifted right, sandy color scheme applied to text
    
    * sandy color applied to all other home screen elemnts.
    
    * REF-3: Name Change, HomeScreen.js Depricated, HomeScreen.js -> HomeScreen_Dep.js
    
    * REF-3: react-native-size-matters scaling package installed
    
    * REF-3: Minor Scaling Applied
    
    * REF-3: Further Rescaling, to suit alt devices
    
    * REF-3: Further Rescaling
    
    * REF-3: Scaling Ready for Pre-Testing
    
    * REF-3: Vertical Scaling and Moderate Scaling added
    
    * Keyboard View, Rescaling ready for test
    
    * Poppins Bold and Light added
    
    * REF-3: More Code Cleanup
    
    * REF-3: More Code Cleanup
    
    * REF-3: More Code Cleanup
    
    * REF-3: More Code Cleanup
    
    * REF-3: input box adjustment
    
    * REF-3: Changes to scaling, added small right margin
    
    * REF-3: reformatted code to specs
    
    * REF-3: Code Restructure
    
    * REF-3: Left Margin scaling fix
    
    * REF-3: Minor Margin Fix, headline text 1 element.
    
    * REF-3: fixing checkNum() on input

--- src/components/main/HomeScreen.js
+++ src/components/main/HomeScreen.js
@@ -1,95 +1,53 @@
-import React from 'react'
+import React from "react";
 
 import {
-  TouchableOpacity,
   TouchableWithoutFeedback,
   StyleSheet,
   Dimensions,
   Text,
   Keyboard,
   View,
-  ImageBackground
-} from 'react-native'
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
-import { Linking } from 'expo';
+import { Input } from "react-native-elements";
 
-import { Input } from 'react-native-elements'
+import { scale, verticalScale, moderateScale } from "react-native-size-matters";
 
-import { NavigationEvents } from 'react-navigation'
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import COLORS from "../../assets/Colors.js";
 
-import COLORS from '../../assets/Colors.js'
+import MenuBar from "./MenuBar.js";
 
-import MenuBar from './MenuBar.js'
+import i18n from "i18n-js";
 
-import Auth from '@aws-amplify/auth'
-
-import i18n from 'i18n-js'
-
-import * as CONSTANTS from '../utilities/Constants.js'
-
-const images = [
-  require('../../assets/background/home/home-1.png'),
-  require('../../assets/background/home/home-2.png'),
-  require('../../assets/background/home/home-3.png'),
-  require('../../assets/background/home/home-4.png'),
-  require('../../assets/background/home/home-5.png')
-]
+const background_image = require("../../assets/background/home/bg_home.png");
 
 export default class HomeScreen extends React.Component {
   state = {
-    isAuthenticated: false,
-    flight: '',
+    flight: "",
     error: false,
-  }
+  };
 
   // load background
   constructor(props) {
-    super(props)
-
-    const randomNumber = Math.floor(Math.random() * 5);
-    this.background = images[randomNumber]
-
-    this.selectBackground(images)
+    super(props);
+    this.background = background_image;
   }
 
   onChangeText(key, value) {
-    this.setState({
-      [key]: value
-    })
-  }
-
-  // selects a random background to be loaded
-  selectBackground() {
-    const randomNumber = Math.floor(Math.random() * 5);
-    this.background = images[randomNumber]
-  }
-
-  // Opens up email to bug-report
-  handleBugReports = () => {
-    Linking.openURL('mailto:bug-report@refloresta.app?body=' + CONSTANTS.BUG_REPORT);
-  }
-
-  // Checks if a user is logged in
-  async checkAuth() {
-    await Auth.currentAuthenticatedUser({ bypassCache: true })
-      .then(() => {
-        console.log('A user is logged in')
-        this.setState({ isAuthenticated: true })
-      })
-      .catch(err => {
-        console.log('Nobody is logged in')
-        this.setState({ isAuthenticated: false })
-      })
+    this.setState({ [key]: value });
   }
 
   // Check if the inputed flight number is valid
   checkNum() {
-    let spaceBuffer = this.state.flight.replace(/\s+/g, '');
+    let spaceBuffer = this.state.flight.replace(/\s+/g, "");
     this.setState({
-      flight: spaceBuffer
-    })
+      flight: spaceBuffer,
+    });
     let charsIata = spaceBuffer.slice(0, 2).toUpperCase();
     let charsIcao = spaceBuffer.slice(0, 3).toUpperCase();
     console.log(charsIata);
@@ -108,248 +66,243 @@ export default class HomeScreen extends React.Component {
 
   // checks for valid Iata
   iataCall(chars, nums) {
-    fetch(`http://aviation-edge.com/v2/public/routes?key=760fd0-cefe7a&airlineIata=${chars}&flightnumber=${nums}`, {
-      method: 'GET'
-    })
+    fetch(
+      `http://aviation-edge.com/v2/public/routes?key=760fd0-cefe7a&airlineIata=${chars}&flightnumber=${nums}`,
+      {
+        method: "GET",
+      }
+    )
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
           validNum: true,
           data: responseJson[0],
-        })
+        });
+
         console.log(this.state.data);
+
         if (!this.state.data) {
-          this.setState({ validNum: false })
+          this.setState({ validNum: false });
         }
+
         return this.state.validNum;
-      }).then((validNum) => {
+      })
+      .then((validNum) => {
         if (validNum) {
           flightSearch.current.clear();
-          this.setState({ error: false })
-          this.props.navigation.navigate('FlightInfo', { flightNum: this.state.flight })
+          this.setState({ error: false });
+          this.props.navigation.navigate("FlightInfo", {
+            flightNum: this.state.flight,
+          });
         } else {
-          this.setState({ error: true })
+          this.setState({ error: true });
           flightSearch.current.shake();
           flightSearch.current.clear();
         }
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.error(error);
       });
   }
 
   // checks for valid Icao
   icaoCall(chars, nums) {
-    fetch(`http://aviation-edge.com/v2/public/routes?key=760fd0-cefe7a&airlineIcao=${chars}&flightnumber=${nums}`, {
-      method: 'GET'
-    })
+    fetch(
+      `http://aviation-edge.com/v2/public/routes?key=760fd0-cefe7a&airlineIcao=${chars}&flightnumber=${nums}`,
+      {
+        method: "GET",
+      }
+    )
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
           validNum: true,
           data: responseJson[0],
-        })
+        });
         console.log(this.state.data);
         if (!this.state.data) {
-          this.setState({ validNum: false })
+          this.setState({ validNum: false });
         }
         return this.state.validNum;
-      }).then((validNum) => {
+      })
+      .then((validNum) => {
         if (validNum) {
           flightSearch.current.clear();
-          this.setState({ error: false })
-          this.props.navigation.navigate('FlightInfo', { flightNum: this.state.flight })
+          this.setState({ error: false });
+          this.props.navigation.navigate("FlightInfo", {
+            flightNum: this.state.flight,
+          });
         } else {
-          this.setState({ error: true })
+          this.setState({ error: true });
           flightSearch.current.shake();
           flightSearch.current.clear();
         }
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.error(error);
       });
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <ImageBackground source={this.background} style={styles.imageBackground}>
-          <TouchableWithoutFeedback style={styles.container} onPress={Keyboard.dismiss}>
-            <View style={styles.container}>
-              <View style={styles.containerTop}>
-                {/* Update isAuthenticated on navigation refresh */}
-                <NavigationEvents onWillFocus={() => { this.checkAuth(); this.selectBackground(); }} />
-                <View style={styles.buttonBarNav}>
-                  {/* <Text style={styles.bigGreenText}>&#x2013; &#x2013;</Text> //To be used after beta */}
-                  {/* Bug Reports to be used only for beta */}
-                  <TouchableOpacity activeOpacity={0.9}
-                    onPress={() => this.handleBugReports()}
-                    style={styles.navStyle}>
-                    <Ionicons style={styles.navigationIcon} name="ios-bug" />
-                  </TouchableOpacity>
-                  {/* isAuthenticated: false */}
-                  {!this.state.isAuthenticated &&
-                    <TouchableOpacity activeOpacity={0.9}
-                      onPress={() => this.props.navigation.navigate('SignIn')}
-                      style={styles.navStyle}>
-                      <FontAwesome style={styles.navigationIcon} name="user-circle-o" />
-                      <Text style={styles.navText}>
-                        {i18n.t('SIGN IN')}
-                      </Text>
-                    </TouchableOpacity>
-                  }
-                  {/* isAuthenticated: true */}
-                  {this.state.isAuthenticated &&
-                    <TouchableOpacity activeOpacity={0.9}
-                      onPress={() => this.props.navigation.navigate('UserProfile')}
-                      style={styles.navStyle}>
-                      <FontAwesome style={styles.navigationIcon} name="user-circle-o" />
-                      <Text style={styles.navText}>
-                        {i18n.t('PROFILE')}
-                      </Text>
-                    </TouchableOpacity>
-                  }
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : null}
+          enabled="false"
+          style={styles.container}>
+          <ImageBackground
+            source={this.background}
+            style={styles.imageBackground}>
+            <View>
+              <View style={styles.textContainer}>
+                {/* Intro Text Body View */}
+                <View>
+                  <Text style={styles.largeWhiteText}>
+                    A cada $6 uma árvore é plantada.
+                  </Text>
+
+                  <Text style={styles.mediumWhiteText}>
+                    Faça sua doação e ajude a recuperar a Mata Atlântica da
+                    Fazenda do Bulcão.
+                  </Text>
+
+                  <Text style={styles.smallWhiteText}>
+                    Insira o número de vôo para iniciar ou
+                  </Text>
+
+                  <Text style={styles.linkWhiteText}>
+                    doe sem número de vôo{" "}
+                    <MaterialCommunityIcons
+                      name="chevron-double-right"
+                      style={styles.chevronIcon}
+                      onPress={() =>
+                        this.props.navigation.navigate("CheckoutWithoutFlight")
+                      }></MaterialCommunityIcons>
+                  </Text>
                 </View>
-                <View style={styles.appName}>
-                  <Text style={styles.bigWhiteText}>refloresta</Text>
-                </View>
+
                 <View style={styles.searchContainer}>
                   {/* Enter flight number */}
                   <Input
                     containerStyle={styles.containerStyle}
                     inputContainerStyle={styles.inputContainerStyle}
                     inputStyle={styles.inputStyle}
-                    label={i18n.t('FLIGHT NUMBER')}
-                    labelStyle={styles.labelStyle}
                     rightIcon={
-                      <Ionicons style={styles.searchIcon}
+                      <Ionicons
+                        style={styles.searchIcon}
                         name="md-arrow-forward"
-                        onPress={() => this.checkNum()} />
+                        onPress={() => this.checkNum()}
+                      />
                     }
-                    rightIconContainerStyle={styles.rightIconContainerStyle}
-                    errorMessage={i18n.t('Please enter a valid flight number')}
-                    errorStyle={[{ fontSize: (this.state.error == false) ? 3 : 10 }, { color: (this.state.error == false) ? 'transparent' : 'red' }]}
-                    autoCapitalize='characters'
+                    errorMessage={i18n.t("Please enter a valid flight number")}
+                    errorStyle={[
+                      {
+                        fontSize:
+                          this.state.error == false ? scale(3) : scale(10),
+                      },
+                      {
+                        color:
+                          this.state.error == false
+                            ? "transparent"
+                            : COLORS.sandy,
+                      },
+                    ]}
+                    autoCapitalize="characters"
                     autoCorrect={false}
                     ref={flightSearch}
-                    onChangeText={value => this.onChangeText('flight', value)}
+                    onChangeText={(value) => this.onChangeText("flight", value)}
                   />
                 </View>
-                {/* Redirect to donation checkout */}
-                {/* NAVIGATION FOR TESTING ONLY */}
-                <TouchableOpacity activeOpacity={0.9}
-                  style={styles.bottomGreenButton}
-                  onPress={() => this.props.navigation.navigate('CheckoutWithoutFlight')}>
-                  <Text style={styles.buttonText}>
-                    {i18n.t('DONATE WITH NO FLIGHT NUMBER')}
-                  </Text>
-                </TouchableOpacity>
               </View>
             </View>
-          </TouchableWithoutFeedback>
-        </ImageBackground>
-        <MenuBar navigation = {this.props.navigation}/>
-      </View>
+            <MenuBar navigation={this.props.navigation} />
+          </ImageBackground>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
 const flightSearch = React.createRef();
 
-const { width, height } = Dimensions.get('screen');
+const { width, height } = Dimensions.get("screen");
 
 const styles = StyleSheet.create({
   imageBackground: {
-    height: height,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    height: Math.round(scale(height)),
     width: width,
-    backgroundColor: COLORS.black
+    backgroundColor: COLORS.black,
   },
   container: {
     flex: 1,
-    flexDirection: 'column',
-    height: height,
-    width: width,
-    backgroundColor: 'transparent',
+    flexDirection: "column",
+    backgroundColor: "transparent",
   },
-  containerTop: {
-    paddingLeft: width * 0.05,
-    paddingRight: width * 0.05,
-    paddingTop: height * 0.06,
-    marginBottom: height * 0.10,
-    backgroundColor: 'transparent',
+  textContainer: {
+    marginLeft: Math.round(moderateScale(120, 0.625)),
+    marginRight: Math.round(moderateScale(10, 0.0625)),
+    backgroundColor: "transparent",
   },
-  buttonBarNav: {
-    flexDirection: 'row',
-    height: height * 0.05,
-    justifyContent: 'space-between',
-    backgroundColor: 'transparent',
+  largeWhiteText: {
+    fontSize: Math.round(moderateScale(24, 0.05)),
+    fontFamily: "Poppins-bold",
+    color: COLORS.sandy,
   },
-  navStyle: {
-    flexDirection: 'column',
-    alignItems: 'center'
+  mediumWhiteText: {
+    color: COLORS.sandy,
+    fontSize: Math.round(scale(20, 0.00125)),
+    fontFamily: "Poppins-light",
   },
-  navigationIcon: {
-    color: COLORS.lightgreen,
-    fontSize: 30,
+  smallWhiteText: {
+    color: COLORS.sandy,
+    fontSize: Math.round(scale(9, 0.625)),
+    fontFamily: "Poppins-light",
   },
-  navText: {
-    fontWeight: 'bold',
-    fontSize: 12,
-    color: COLORS.lightgreen,
-  },
-  bigGreenText: {
-    fontWeight: 'bold',
-    fontSize: 40,
-    lineHeight: 45,
-    color: COLORS.lightgreen,
-  },
-  appName: {
-    paddingTop: height * 0.2,
-  },
-  bigWhiteText: {
-    fontSize: 50,
-    fontFamily: 'Gilroy-bold',
-    color: COLORS.white,
-    textAlign: 'center'
+  linkWhiteText: {
+    color: COLORS.sandy,
+    fontSize: Math.round(scale(9, 0.625)),
+    fontFamily: "Poppins-bold",
+    textDecorationLine: "underline",
   },
   searchContainer: {
-    paddingTop: height * 0.02,
-    paddingBottom: height * 0.03,
+    marginTop: Math.round(verticalScale(35)),
   },
   containerStyle: {
-    backgroundColor: COLORS.mutedgreen,
-    borderRadius: 15,
-    borderColor: COLORS.lightgreen,
-    borderWidth: 2,
-    paddingTop: 6,
-    opacity: 0.95
+    opacity: 0.95,
+    width: moderateScale(220, 0.5),
+    shadowRadius: 5,
+    shadowOpacity: 0.7,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 5, height: 1 },
+    backgroundColor: "transparent",
+    borderColor: COLORS.sandy,
+    borderWidth: Math.round(scale(3)),
+    borderRadius: Math.round(scale(15)),
   },
   inputContainerStyle: {
-    borderColor: COLORS.white,
-    borderBottomWidth: 2
+    paddingTop: verticalScale(15),
+    borderBottomColor: "transparent",
   },
   inputStyle: {
-    fontSize: 27,
-    color: COLORS.white,
+    color: COLORS.sandy,
+    fontFamily: "Poppins-bold",
+    fontSize: Math.round(moderateScale(30, 0.0125)),
   },
   labelStyle: {
-    fontSize: 10,
-    color: COLORS.white,
+    color: COLORS.sandy,
+    fontSize: Math.round(scale(20)),
   },
   searchIcon: {
-    color: COLORS.white,
-    fontSize: 40,
+    color: COLORS.sandy,
+    textAlign: "center",
+    fontSize: Math.round(scale(35)),
   },
-  rightIconContainerStyle: {
-    paddingBottom: 3
+  chevronIcon: {
+    color: COLORS.sandy,
+    textAlignVertical: "center",
+    textDecorationColor: "transparent",
   },
-  bottomGreenButton: {
-    borderRadius: 10,
-    backgroundColor: COLORS.lightgreen,
-    height: height * 0.04,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: COLORS.darkgrey,
-    fontSize: 12
-  }
-})
+});
