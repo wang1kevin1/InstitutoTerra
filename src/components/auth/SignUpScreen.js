187c927fb56cf821d8fd6fee109bcd384dd24950
George Mbugua <gmbugua2020@gmail.com>
2020-07-06 20:27:25 -0700

    code legibility improved

--- src/components/auth/SignUpScreen.js
+++ src/components/auth/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import {
   TouchableOpacity,
@@ -11,134 +11,134 @@ import {
   View,
   Alert,
   ActivityIndicator,
-} from 'react-native'
+} from "react-native";
 
-import {
-  Container,
-  Item,
-  Input} from 'native-base'
+import { Container, Item, Input } from "native-base";
 
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
-import COLORS from '../../assets/Colors.js'
+import COLORS from "../../assets/Colors.js";
 
-import Auth from '@aws-amplify/auth'
+import Auth from "@aws-amplify/auth";
 
-import { API } from 'aws-amplify'
+import { API } from "aws-amplify";
 
-import i18n from 'i18n-js'
+import i18n from "i18n-js";
 
 export default class SignUpScreen extends React.Component {
   state = {
-    userSub: '',
-    name: '',
-    email: '',
-    password: '',
-    password_confirmation: '',
+    userSub: "",
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
     isLoading: false,
     hidePassword1: true,
     hidePassword2: true,
     apiResponse: null,
-    UserId: ''
-  }
- 
+    UserId: "",
+  };
+
   handleChangeUserId = (event) => {
-      this.setState({UserId: event});
-  }
-  
+    this.setState({ UserId: event });
+  };
+
   onChangeText(key, value) {
     this.setState({
-      [key]: value
-    })
+      [key]: value,
+    });
   }
 
-  // toggles secure text password 
+  // toggles secure text password
   handleHidePassword1 = () => {
     if (this.state.hidePassword1) {
-      this.setState({hidePassword1: false})
+      this.setState({ hidePassword1: false });
     } else {
-      this.setState({hidePassword1: true})
+      this.setState({ hidePassword1: true });
     }
-  }
+  };
 
   // toggles secure text password confirmation
   handleHidePassword2 = () => {
     if (this.state.hidePassword2) {
-      this.setState({hidePassword2: false})
+      this.setState({ hidePassword2: false });
     } else {
-      this.setState({hidePassword2: true})
+      this.setState({ hidePassword2: true });
     }
-  }
+  };
 
   // checks for password match
   handleSignUp = () => {
     if (this.state.password !== this.state.password_confirmation) {
-      Alert.alert(i18n.t('Passwords do not match'))
+      Alert.alert(i18n.t("Passwords do not match"));
     } else {
-      this.signUp()
+      this.signUp();
     }
-  }
+  };
 
   // Sign up user with AWS Amplify Auth
   async signUp() {
-    Keyboard.dismiss()
-    this.setState({ isLoading: true })
+    Keyboard.dismiss();
+    this.setState({ isLoading: true });
     await Auth.signUp({
       username: this.state.email,
       password: this.state.password,
-      attributes: { 
+      attributes: {
         email: this.state.email,
         name: this.state.name,
-      }
+      },
     })
-    .then(data => {
-      // grab user unique sub
-      console.log('sign up successful with result: ', data)
-      this.setState({data})
-      this.setState({userSub: data.userSub})
-      console.log(this.state.userSub)
-      Alert.alert(i18n.t('An email has been sent to confirm your sign up'))
-      this.saveUser()
-    })
-    .catch(err => {
-      this.setState({ isLoading: false })
-      if (! err.message) {
-        console.log('Error when signing up: ', err)
-        Alert.alert(i18n.t('Error when signing up: '), err)
-      } else {
-        console.log('Error when signing up: ', err.message)
-        Alert.alert(i18n.t('Error when signing up: '), err.message)
-      }
-    })
+      .then((data) => {
+        // grab user unique sub
+        console.log("sign up successful with result: ", data);
+        this.setState({ data });
+        this.setState({ userSub: data.userSub });
+        console.log(this.state.userSub);
+        Alert.alert(i18n.t("An email has been sent to confirm your sign up"));
+        this.saveUser();
+      })
+      .catch((err) => {
+        this.setState({ isLoading: false });
+        if (!err.message) {
+          console.log("Error when signing up: ", err);
+          Alert.alert(i18n.t("Error when signing up: "), err);
+        } else {
+          console.log("Error when signing up: ", err.message);
+          Alert.alert(i18n.t("Error when signing up: "), err.message);
+        }
+      });
   }
 
   // adds user to Amplify database
   async saveUser() {
     let newUser = {
       body: {
-        "UserId": this.state.userSub,
-        "TreesPlanted": 0,
-      }
-    }
+        UserId: this.state.userSub,
+        TreesPlanted: 0,
+      },
+    };
     const path = "/Users";
 
     // Use the API module to save the note to the database
     await API.put("ZeroCarbonREST", path, newUser)
-      .then(apiResponse => {
-        this.setState({apiResponse});
+      .then((apiResponse) => {
+        this.setState({ apiResponse });
         console.log("Response from saving user: " + apiResponse);
-        this.setState({ isLoading: false })
-        this.props.navigation.navigate('SignIn')
+        this.setState({ isLoading: false });
+        this.props.navigation.navigate("SignIn");
       })
-      .catch(e => {
-      console.log(e);
-    })
+      .catch((e) => {
+        console.log(e);
+      });
   }
-  
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior="padding"
+          enabled>
           <TouchableWithoutFeedback
             style={styles.container}
             onPress={Keyboard.dismiss}>
@@ -150,13 +150,15 @@ export default class SignUpScreen extends React.Component {
                     <Ionicons style={styles.iconStyle1} name="ios-person" />
                     <Input
                       style={styles.input}
-                      placeholder={i18n.t('Name')}
+                      placeholder={i18n.t("Name")}
                       placeholderTextColor={COLORS.lightblue}
-                      returnKeyType='next'
-                      autoCapitalize='none'
+                      returnKeyType="next"
+                      autoCapitalize="none"
                       autoCorrect={false}
-                      onSubmitEditing={(event) => { this.refs.SecondInput._root.focus() }}
-                      onChangeText={value => this.onChangeText('name', value)}
+                      onSubmitEditing={(event) => {
+                        this.refs.SecondInput._root.focus();
+                      }}
+                      onChangeText={(value) => this.onChangeText("name", value)}
                     />
                   </Item>
                   {/* Email */}
@@ -164,15 +166,19 @@ export default class SignUpScreen extends React.Component {
                     <Ionicons style={styles.iconStyle1} name="ios-mail" />
                     <Input
                       style={styles.input}
-                      placeholder={i18n.t('Email')}
+                      placeholder={i18n.t("Email")}
                       placeholderTextColor={COLORS.lightblue}
-                      returnKeyType='next'
-                      autoCapitalize='none'
+                      returnKeyType="next"
+                      autoCapitalize="none"
                       autoCorrect={false}
-                      keyboardType={'email-address'}
-                      ref='SecondInput'
-                      onSubmitEditing={(event) => { this.refs.ThirdInput._root.focus() }}
-                      onChangeText={value => this.onChangeText('email', value)}
+                      keyboardType={"email-address"}
+                      ref="SecondInput"
+                      onSubmitEditing={(event) => {
+                        this.refs.ThirdInput._root.focus();
+                      }}
+                      onChangeText={(value) =>
+                        this.onChangeText("email", value)
+                      }
                     />
                   </Item>
                   {/* Password */}
@@ -180,78 +186,94 @@ export default class SignUpScreen extends React.Component {
                     <Ionicons style={styles.iconStyle1} name="ios-lock" />
                     <Input
                       style={styles.input}
-                      placeholder={i18n.t('Password')}
+                      placeholder={i18n.t("Password")}
                       placeholderTextColor={COLORS.lightblue}
-                      returnKeyType='next'
-                      autoCapitalize='none'
+                      returnKeyType="next"
+                      autoCapitalize="none"
                       autoCorrect={false}
                       secureTextEntry={this.state.hidePassword1}
-                      ref='ThirdInput'
-                      onSubmitEditing={(event) => { this.refs.FourthInput._root.focus() }}
-                      onChangeText={value => this.onChangeText('password', value)}
+                      ref="ThirdInput"
+                      onSubmitEditing={(event) => {
+                        this.refs.FourthInput._root.focus();
+                      }}
+                      onChangeText={(value) =>
+                        this.onChangeText("password", value)
+                      }
                     />
-                    <Ionicons style={styles.iconStyle2} name="ios-eye" onPress={() => this.handleHidePassword1()}/>
+                    <Ionicons
+                      style={styles.iconStyle2}
+                      name="ios-eye"
+                      onPress={() => this.handleHidePassword1()}
+                    />
                   </Item>
                   {/* Confirm Password */}
                   <Item style={styles.itemStyle}>
                     <Ionicons style={styles.iconStyle1} name="ios-lock" />
                     <Input
                       style={styles.input}
-                      placeholder={i18n.t('Confirm Password')}
+                      placeholder={i18n.t("Confirm Password")}
                       placeholderTextColor={COLORS.lightblue}
-                      returnKeyType='go'
-                      autoCapitalize='none'
+                      returnKeyType="go"
+                      autoCapitalize="none"
                       autoCorrect={false}
                       secureTextEntry={this.state.hidePassword2}
-                      ref='FourthInput'
-                      onChangeText={value => this.onChangeText('password_confirmation', value)}
+                      ref="FourthInput"
+                      onChangeText={(value) =>
+                        this.onChangeText("password_confirmation", value)
+                      }
                     />
-                    <Ionicons style={styles.iconStyle2} name="ios-eye" onPress={() => this.handleHidePassword2()}/>
+                    <Ionicons
+                      style={styles.iconStyle2}
+                      name="ios-eye"
+                      onPress={() => this.handleHidePassword2()}
+                    />
                   </Item>
                   {/* SignUp Button */}
                   <TouchableOpacity
                     onPress={() => this.handleSignUp()}
                     disabled={this.state.isLoading}
                     style={styles.buttonStyle1}>
-                    <Text style={styles.buttonText1}>
-                      {i18n.t('Sign Up')}
-                    </Text>
+                    <Text style={styles.buttonText1}>{i18n.t("Sign Up")}</Text>
                   </TouchableOpacity>
                   {/* Loading ActivityIndicator */}
-                  {this.state.isLoading &&
+                  {this.state.isLoading && (
                     <View>
-                      <ActivityIndicator color={COLORS.lightblue} size='large' animating={this.state.isLoading} />
+                      <ActivityIndicator
+                        color={COLORS.lightblue}
+                        size="large"
+                        animating={this.state.isLoading}
+                      />
                     </View>
-                  }
+                  )}
                 </View>
               </Container>
             </View>
           </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
       </SafeAreaView>
-    )
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.lightgreen,
-    justifyContent: 'center',
-    flexDirection: 'column'
+    justifyContent: "center",
+    flexDirection: "column",
   },
   input: {
     flex: 1,
     fontSize: 17,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: COLORS.lightblue,
   },
   infoContainer: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 30,
     backgroundColor: COLORS.lightgreen,
   },
@@ -259,24 +281,24 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     backgroundColor: COLORS.white,
     borderRadius: 10,
-    borderColor: 'transparent'
+    borderColor: "transparent",
   },
   iconStyle1: {
     color: COLORS.lightblue,
     fontSize: 30,
     marginRight: 15,
     marginLeft: 15,
-    flex: 0.1
+    flex: 0.1,
   },
   iconStyle2: {
     color: COLORS.grey,
     fontSize: 20,
     marginRight: 15,
     marginLeft: 15,
-    flex: 0.1
+    flex: 0.1,
   },
   buttonStyle1: {
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: COLORS.lightblue,
     padding: 14,
     marginBottom: 20,
@@ -284,7 +306,7 @@ const styles = StyleSheet.create({
   },
   buttonText1: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: COLORS.white,
   },
-})
+});
