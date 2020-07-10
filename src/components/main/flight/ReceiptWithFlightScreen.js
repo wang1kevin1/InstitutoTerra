797132e86906fbdf5ddbf1289f3493b2268a4be6
George Mbugua <gmbugua2020@gmail.com>
2020-07-10 00:54:09 -0700

    REF-5: New UI Implemented, need Flight Distance Data.

--- src/components/main/flight/ReceiptWithFlightScreen.js
+++ src/components/main/flight/ReceiptWithFlightScreen.js
@@ -6,16 +6,14 @@ import {
   TouchableOpacity,
   Keyboard,
   Image,
+  SafeAreaView,
 } from "react-native";
-import { scale, verticalScale, moderateScale } from "react-native-size-matters";
-import { Form, Item, Input, Label } from "native-base";
-import { Ionicons, FontAwesome } from "@expo/vector-icons";
-import Dash from "react-native-dash";
+import { verticalScale, moderateScale } from "react-native-size-matters";
+import { List, ListItem, Left, Right } from "native-base";
 import COLORS from "../../../assets/Colors.js";
 import MenuBar from "../MenuBar.js";
 import Auth from "@aws-amplify/auth";
 import i18n from "i18n-js";
-import { SafeAreaView } from "react-navigation";
 
 const leaf = require("../../../assets/images/img_receipt_leaf_nobg.png");
 
@@ -87,29 +85,96 @@ export default class ReceiptWithFlightScreen extends React.Component {
     return (
       <SafeAreaView style={styles.backDrop}>
         <View style={styles.innerView}>
-          <Text
-            style={{
-              color: COLORS.forestgreen,
-              textAlign: "center",
-              fontFamily: "Poppins",
-              fontSize: Math.round(moderateScale(16, 0.0625)),
-            }}>
-            Flight Number {""}
-            <Text style={{ fontFamily: "Poppins-bold" }}>
-              {flightChars} {flightNums}
+          <View style={styles.topInnerView}>
+            <Text style={styles.flightNumberLabel}>
+              Flight Number {""}
+              <Text style={{ fontFamily: "Poppins-bold" }}>
+                {flightChars} {flightNums}
+              </Text>
             </Text>
-          </Text>
-          <Text
-            style={{
-              color: COLORS.forestgreen,
-              textAlign: "center",
-              fontFamily: "Poppins-bold",
-              fontSize: Math.round(moderateScale(18, 0.0625)),
-              marginTop: Math.round(verticalScale(20)),
-            }}>
-            The first step was taken. The rest is up to us!
-          </Text>
-          <Image source={leaf} style={styles.leaf_img} />
+            <Text style={styles.header}>
+              The first step was taken. The rest is up to us!
+            </Text>
+            <Image source={leaf} style={styles.leaf_img} />
+          </View>
+          <View style={styles.bottomInnerView}>
+            <List>
+              {/* List Item 1 */}
+              <ListItem>
+                <Left>
+                  <Text style={styles.itemTitle}>Flight Distance (km)</Text>
+                </Left>
+                <Right>
+                  <Text style={styles.itemValue}></Text>
+                </Right>
+              </ListItem>
+              {/* List Item 2 */}
+              <ListItem>
+                <Left>
+                  <Text style={styles.itemTitle}>
+                    Tons of CO
+                    <Text
+                      style={{
+                        fontSize: moderateScale(7, 0.125),
+                        lineHeight: 1,
+                      }}>
+                      2
+                    </Text>
+                  </Text>
+                </Left>
+                <Right>
+                  <Text style={styles.itemValue}>{footprint}</Text>
+                </Right>
+              </ListItem>
+              {/* List Item 3 */}
+              <ListItem>
+                <Left>
+                  <Text style={styles.itemTitle}>Planted Trees</Text>
+                </Left>
+                <Right>
+                  <Text style={styles.itemValue}>{treeNum}</Text>
+                </Right>
+              </ListItem>
+              {/* List Item 4 */}
+              <ListItem>
+                <Left>
+                  <Text style={styles.itemTitle}>Years to Compensate</Text>
+                </Left>
+                <Right>
+                  <Text style={styles.itemValue}>{years}</Text>
+                </Right>
+              </ListItem>
+              {/* List Item 5 */}
+              <ListItem>
+                <Left>
+                  <Text style={styles.itemTitle}>Donation Amount</Text>
+                </Left>
+                <Right>
+                  <Text style={styles.itemValue}>${total_cost}</Text>
+                </Right>
+              </ListItem>
+            </List>
+          </View>
+          {/* SignUp Button */}
+          <TouchableOpacity
+            onPress={() => this.handleStripePayment()}
+            disabled={this.state.isLoading}
+            style={styles.submitButton}>
+            {/* Not Loading Hide ActivityIndicator */}
+            {!this.state.isLoading && (
+              <Text style={styles.submitLabel}>Pay With Stripe</Text>
+            )}
+            {/* Show Loading ActivityIndicator */}
+            {this.state.isLoading && (
+              <View styles={styles.loading}>
+                <ActivityIndicator
+                  color={COLORS.sandy}
+                  size="large"
+                  animating={this.state.isLoading}
+                />
+              </View>
+            )}
+          </TouchableOpacity>
         </View>
         <MenuBar navigation={this.props.navigation} />
       </SafeAreaView>
@@ -128,8 +193,29 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     marginLeft: Math.round(moderateScale(105, 0.625)),
     marginRight: Math.round(moderateScale(20, 0.0625)),
-    marginBottom: Math.round(moderateScale(10, 0.0625)),
+    marginBottom: Math.round(moderateScale(15, 0.0625)),
     marginTop: Math.round(moderateScale(20, 0.0625)),
+  },
+  topInnerView: {
+    flex: 1 / 2,
+    marginBottom: verticalScale(10),
+  },
+  bottomInnerView: {
+    flex: 1 / 2,
+    marginTop: verticalScale(30),
+  },
+  flightNumberLabel: {
+    color: COLORS.forestgreen,
+    textAlign: "center",
+    fontFamily: "Poppins",
+    fontSize: Math.round(moderateScale(16, 0.0625)),
+  },
+  header: {
+    color: COLORS.forestgreen,
+    textAlign: "center",
+    fontFamily: "Poppins-bold",
+    fontSize: Math.round(moderateScale(18, 0.0625)),
+    marginTop: Math.round(verticalScale(20)),
   },
   leaf_img: {
     width: "100%",
@@ -142,5 +228,36 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.53,
     shadowRadius: 20,
+  },
+  itemTitle: {
+    color: COLORS.forestgreen,
+    fontSize: Math.round(moderateScale(13, 0.125)),
+    fontFamily: "Poppins",
+  },
+  itemValue: {
+    color: COLORS.forestgreen,
+    fontSize: Math.round(moderateScale(13, 0.125)),
+    fontFamily: "Poppins-bold",
+  },
+  submitButton: {
+    alignItems: "center",
+    borderRadius: 10,
+    padding: Math.round(verticalScale(10)),
+    backgroundColor: COLORS.forestgreen,
+  },
+  loading: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  submitLabel: {
+    color: COLORS.sandy,
+    fontSize: Math.round(moderateScale(20, 0.05)),
+    padding: Math.round(moderateScale(10, 0.0125)),
+    fontFamily: "Poppins-bold",
   },
 });
